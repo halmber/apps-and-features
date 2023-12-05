78f80341e2867131d3aa860eed871cec7409e666
Bohdan <rogozyanskiy.bogdan@gmail.com>
2023-12-06 01:24:06 +0200

    created executeQuery function to query several registers

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const REGISTER_1 = "HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall";
+const REGISTER_2 = "HKLM\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall";
+
+module.exports = {
+    REGISTER_1,
+    REGISTER_2,
+};
--- src/getInstalledSoftware.js
+++ src/getInstalledSoftware.js
@@ -1,53 +1,63 @@
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
+const { REGISTER_1, REGISTER_2 } = require("./constants");
 
-const getInstalledSoftware = async () => {
+const executeQuery = async (strQuery) => {
     try {
-        const { stdout } = await exec("reg query HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall /s");
-        const lines = stdout
+        const { stdout } = await exec(`reg query ${strQuery} /s`);
+
+        return stdout
             .split("\n")
             .map((line) => line.trim())
             .filter((item) => Boolean(item));
-
-        const softwareList = [];
-        let currentSoftware = {};
-
-        for (const line of lines) {
-            const displayNameMatch = line.match(/DisplayName\s+REG_SZ\s+(.*)/);
-            const displayIconMatch = line.match(/DisplayIcon\s+REG_SZ\s+(.*)/);
-
-            // If a new register is started, we add the current already recorded application to the list
-            if (line.startsWith("HKEY_LOCAL_MACHINE")) {
-                if (currentSoftware.DisplayName && currentSoftware.DisplayIcon) {
-                    softwareList.push({
-                        name: currentSoftware.DisplayName,
-                        installLocation: currentSoftware.DisplayIcon.replace(/,.*$/, ""),
-                    });
-
-                    currentSoftware = {};
-                }
-            }
-
-            if (displayNameMatch) {
-                currentSoftware.DisplayName = displayNameMatch[1].trim();
-            }
-
-            if (displayIconMatch) {
-                currentSoftware.DisplayIcon = displayIconMatch[1].trim();
-            }
-        }
-        if (currentSoftware.DisplayName && currentSoftware.DisplayIcon) {
-            softwareList.push({
-                name: currentSoftware.DisplayName,
-                installLocation: currentSoftware.DisplayIcon.replace(/,.*$/, ""),
-            });
-        }
-
-        return softwareList.sort((a, b) => a.name.localeCompare(b.name));
     } catch (error) {
-        console.error("Error getting installed software:", error);
+        console.error("Error getting installed software: \n", error);
         return [];
     }
+};
+
+const getInstalledSoftware = async () => {
+    const [registersLinesPart1, registersLinesPart2] = await Promise.all([
+        executeQuery(REGISTER_1),
+        executeQuery(REGISTER_2),
+    ]);
+    const allRegistersLines = [...registersLinesPart1, ...registersLinesPart2];
+
+    const softwareList = [];
+    let currentSoftware = {};
+
+    for (const line of allRegistersLines) {
+        const displayNameMatch = line.match(/DisplayName\s+REG_SZ\s+(.*)/);
+        const displayIconMatch = line.match(/DisplayIcon\s+REG_SZ\s+(.*)/);
+
+        // If a new register is started, we add the current already recorded application to the list
+        if (line.startsWith("HKEY_LOCAL_MACHINE")) {
+            if (currentSoftware.DisplayName && currentSoftware.DisplayIcon) {
+                softwareList.push({
+                    name: currentSoftware.DisplayName,
+                    installLocation: currentSoftware.DisplayIcon.replace(/,.*$/, ""),
+                });
+
+                currentSoftware = {};
+            }
+        }
+
+        if (displayNameMatch) {
+            currentSoftware.DisplayName = displayNameMatch[1].trim();
+        }
+
+        if (displayIconMatch) {
+            currentSoftware.DisplayIcon = displayIconMatch[1].trim();
+        }
+    }
+    if (currentSoftware.DisplayName && currentSoftware.DisplayIcon) {
+        softwareList.push({
+            name: currentSoftware.DisplayName,
+            installLocation: currentSoftware.DisplayIcon.replace(/,.*$/, ""),
+        });
+    }
+
+    return softwareList.sort((a, b) => a.name.localeCompare(b.name));
 };
 
 module.exports = {
